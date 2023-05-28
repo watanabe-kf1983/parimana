601c8e5193766880cea33855aca717aa56bb0f80
Kiyofumi Watanabe <watanabe.kf1983@gmail.com>
2023-05-28 19:59:35 +0900

    Improve driver wait

--- parimana/scrape/netkeiba/browse.py
+++ parimana/scrape/netkeiba/browse.py
@@ -5,8 +5,6 @@ from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.expected_conditions import (
     all_of,
-    text_to_be_present_in_element,
-    visibility_of_element_located,
 )
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
@@ -27,8 +25,9 @@ def _browse_odds_by_btype(
     driver: WebDriver, race_id: str, btype: BettingType
 ) -> Iterator[str]:
     uri = _odds_page_uri(race_id, btype)
+    print(f"opening {uri} ...", end=" ", flush=True)
     driver.get(uri)
-    print(f"page {uri} got")
+    print("done.", flush=True)
 
     if btype.size < 3:
         yield driver.page_source
@@ -39,10 +38,11 @@ def _browse_odds_by_btype(
         axis_numbers = {elem.get_attribute("value") for elem in options}
 
         for axis in axis_numbers:
+            print(f" downloading axis {axis} odds...", end=" ", flush=True)
             dropdown = driver.find_element(By.CSS_SELECTOR, "#list_select_horse")
             Select(dropdown).select_by_value(axis)
             WebDriverWait(driver, timeout=10).until(_axis_is_loaded(axis))
-            print(f" axis {axis} got")
+            print("done.", flush=True)
             yield driver.page_source
 
 
@@ -51,9 +51,15 @@ def _axis_is_loaded(axis: str):
         text_to_be_present_matched_on_element(
             (By.CSS_SELECTOR, "div.Axis_Horse span.Num"), axis
         ),
-        text_to_be_present_in_element((By.CSS_SELECTOR, "#official_time"), ":"),
-        visibility_of_element_located((By.CSS_SELECTOR, "#official_time")),
+        no_text_in_page_source("---.-"),
     )
+
+
+def no_text_in_page_source(text_):
+    def _predicate(driver):
+        return not (text_ in driver.page_source)
+
+    return _predicate
 
 
 def text_to_be_present_matched_on_element(locator, text_):
