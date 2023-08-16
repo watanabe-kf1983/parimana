334d9c8930880c4b7937cde718fa8a64ca1c28f6
Kiyofumi Watanabe <watanabe.kf1983@gmail.com>
2023-08-16 19:12:44 +0900

    Cache boatrace.jp page

--- parimana/boatrace/browse.py
+++ parimana/boatrace/browse.py
@@ -1,4 +1,5 @@
 from typing import Iterator, Tuple
+import functools
 
 import requests
 
@@ -10,20 +11,24 @@ def browse_odds_pages(
     date: str, cource: int, race_no: int
 ) -> Iterator[Tuple[str, BettingType]]:
     for btype in supported_types:
-        for page_content in _browse_odds_by_btype(date, cource, race_no, btype):
-            yield (page_content, btype)
+        page_content = _browse_odds_by_btype(date, cource, race_no, btype)
+        yield (page_content, btype)
 
 
 def _browse_odds_by_btype(
     date: str, cource: int, race_no: int, btype: BettingType
 ) -> Iterator[str]:
     uri = _odds_page_uri(date, cource, race_no, btype)
+    return _get(uri)
+
+
+@functools.cache
+def _get(uri: str):
     print(f"opening {uri} ...", end=" ", flush=True)
     res = requests.get(uri)
-    print("done.", flush=True)
     res.raise_for_status()
-    yield res.text
-    res.close()
+    print("done.", flush=True)
+    return res.text
 
 
 def _odds_page_uri(date: str, cource: int, race_no: int, btype: BettingType) -> str:
