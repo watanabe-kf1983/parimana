bd0361ea1c7530c7a17bae2827c87fc90b161c71
Kiyofumi Watanabe <watanabe.kf1983@gmail.com>
2023-09-18 22:38:25 +0900

    uncertaintyを1に戻す

--- back/parimana/analyse/ability.py
+++ back/parimana/analyse/ability.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 import pandas as pd
+import scipy.stats
 
 import parimana.analyse.normal_dist as nd
 
@@ -33,7 +34,8 @@ class UMapScore:
 def _evaluate_u_map(corwr_df: pd.DataFrame, u_map: pd.Series) -> UMapScore:
     gap_mtx = _estimate_ability_gap_mtx(corwr_df, u_map)
     gap_mtx_std = gap_mtx.groupby("a").std().rename_axis("m")
-    u_map_suggest = u_map / gap_mtx_std
+    gap_mtx_std_gmean = scipy.stats.mstats.gmean(gap_mtx_std.values)
+    u_map_suggest = u_map * gap_mtx_std_gmean / gap_mtx_std
     score = gap_mtx_std.std()
     return UMapScore(u_map, score, u_map_suggest)
 
