383e2ea002e16780483c2d91df805161a2fa279a
Kiyofumi Watanabe <watanabe.kf1983@gmail.com>
2024-07-17 11:43:13 +0000

    Use Datagrid

--- front/src/features/analysises/components/Candidates.tsx
+++ front/src/features/analysises/components/Candidates.tsx
@@ -1,28 +1,31 @@
 import { Table, TableBody, TableHead, TableRow, TableCell } from '@mui/material';
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 
 import { CandidatesProps } from '../types';
 
+
 export function Candidates(props: CandidatesProps) {
+
+
+    const rows: GridRowsProp = props.data.map(rec => ({
+        id: rec.eye.text,
+        eye: rec.eye.text,
+        type: rec.eye.type,
+        odds: rec.odds.toFixed(1),
+        chance: (rec.chance * 100).toFixed(2),
+        expected: rec.expected.toFixed(4)
+    }));
+
+    const columns: GridColDef[] = [
+        { field: 'eye', headerName: 'Bet', width: 100 },
+        { field: 'odds', headerName: 'Odds', width: 100 },
+        { field: 'chance', headerName: 'Chance', width: 100 },
+        { field: 'expected', headerName: 'Expectation', width: 100 },
+    ];
+
     return (
-        <Table stickyHeader style={{ maxWidth: "400px" }}>
-            <TableHead>
-                <TableRow>
-                    <TableCell>Bet</TableCell>
-                    <TableCell align="right">Odds</TableCell>
-                    <TableCell align="right">Chance</TableCell>
-                    <TableCell align="right">Expectation</TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {props.data.map(rec => (
-                    <TableRow>
-                        <TableCell>{rec.eye.text}</TableCell>
-                        <TableCell align="right">{rec.odds.toFixed(1)}</TableCell>
-                        <TableCell align="right">{(rec.chance * 100).toFixed(2)}%</TableCell>
-                        <TableCell align="right">{rec.expected.toFixed(4)}</TableCell>
-                    </TableRow>
-                ))}
-            </TableBody>
-        </Table>
+        <div style={{ height: 300, maxWidth: '400px' }}>
+            <DataGrid rows={rows} columns={columns} />
+        </div>
     )
 }
