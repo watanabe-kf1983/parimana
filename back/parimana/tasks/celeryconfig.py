117121337e54f30a714b35fa08afb49bc83b3df5
Kiyofumi Watanabe <watanabe.kf1983@gmail.com>
2024-12-29 15:38:11 +0000

    celery result の有効期限を追加

--- back/parimana/tasks/celeryconfig.py
+++ back/parimana/tasks/celeryconfig.py
@@ -1,8 +1,11 @@
+from datetime import timedelta
 import parimana.settings as settings
 
 broker_url = settings.redis_db_uri
-result_backend = settings.redis_db_uri
 event_serializer = "pickle"
 task_serializer = "pickle"
+
+result_backend = settings.redis_db_uri
 result_serializer = "pickle"
+result_expires = timedelta(minute=60)
 accept_content = ["application/json", "application/x-python-serialize"]
