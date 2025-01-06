9693b10b5662172e8790d4d1b8c77419162dd696
Kiyofumi Watanabe <watanabe.kf1983@gmail.com>
2025-01-06 11:47:21 +0000

    バケットポリシーは要らないので削除

--- infra/iac/modules/web/main.tf
+++ infra/iac/modules/web/main.tf
@@ -4,28 +4,3 @@ resource "aws_s3_bucket" "web" {
 
   tags = var.common_tags
 }
-
-
-resource "aws_s3_bucket_policy" "web_policy" {
-  bucket = aws_s3_bucket.web.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = "*"
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject",
-          "s3:ListBucket"
-        ]
-        Resource = [
-          "${aws_s3_bucket.web.arn}/*", # バケット内のオブジェクト
-          "${aws_s3_bucket.web.arn}"    # バケット自体
-        ]
-      }
-    ]
-  })
-}
