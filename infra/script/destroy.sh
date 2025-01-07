ae9d3a3edb9281463d5324029b48388f97249989
Kiyofumi Watanabe <watanabe.kf1983@gmail.com>
2025-01-07 15:37:35 +0000

    Add destroy shell

---
+++ infra/script/destroy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -eux
+
+cd ${CODEBUILD_SRC_DIR}/infra/iac/env/${ENV}
+
+export TF_VAR_aws_region=$AWS_REGION
+export TF_VAR_project_name=$PROJECT_NAME
+export TF_VAR_env=$ENV
+
+terraform destroy
