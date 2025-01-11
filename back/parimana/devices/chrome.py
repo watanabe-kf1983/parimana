d2dca92fe6f828b5296e492106f3b9322cdba963
Kiyofumi Watanabe <watanabe.kf1983@gmail.com>
2025-01-11 17:46:22 +0000

    Set selenium logger level

--- back/parimana/devices/chrome.py
+++ back/parimana/devices/chrome.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Optional
 from selenium import webdriver
 
@@ -7,7 +8,14 @@ _headless_chrome: Optional[webdriver.Chrome] = None
 def headless_chrome() -> webdriver.Chrome:
     global _headless_chrome
     if not _headless_chrome:
+        logger = logging.getLogger("selenium")
+        lev = logger.getEffectiveLevel()
+        logger.setLevel(logging.DEBUG)
+
         _headless_chrome = create_headless_chrome()
+
+        logger.setLevel(lev)
+
     return _headless_chrome
 
 
@@ -17,7 +25,7 @@ def create_headless_chrome() -> webdriver.Chrome:
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
-    
+
     # ChromeDriverがログを吐きまくる対策
     # https://github.com/SeleniumHQ/selenium/issues/13095
     options.add_argument("--log-level=3")
