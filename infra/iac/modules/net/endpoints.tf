079cb6c090ed1e1aaac5de6ebc9d1c97aba36851
Kiyofumi Watanabe <watanabe.kf1983@gmail.com>
2025-01-16 12:01:04 +0000

    Fix S3 endpoint route table

--- infra/iac/modules/net/endpoints.tf
+++ infra/iac/modules/net/endpoints.tf
@@ -2,7 +2,7 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id       = aws_vpc.main.id
   service_name = "com.amazonaws.${var.aws_region}.s3"
   vpc_endpoint_type = "Gateway"
-  route_table_ids = [aws_route_table.public.id]
+  route_table_ids = [aws_route_table.private.id]
   tags = merge(var.common_tags, { Name = "${var.project_name}-${var.env}-vpce-s3" })
 }
 
