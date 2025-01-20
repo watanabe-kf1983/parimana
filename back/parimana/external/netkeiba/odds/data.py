72bf4852ba01e5ebde03e76ca639638c949659a8
Kiyofumi Watanabe <watanabe.kf1983@gmail.com>
2025-01-20 13:13:13 +0000

    Fix keiba betting type ratio

--- back/parimana/external/netkeiba/odds/data.py
+++ back/parimana/external/netkeiba/odds/data.py
@@ -1,19 +1,19 @@
 from parimana.domain.base import BettingType
 
-# ratio_data = {
-#     # https://jra.jp/company/about/financial/pdf/houkoku03.pdf p.26 別表9
-#     BettingType.WIN: 6.9,
-#     BettingType.QUINELLA: 13.3,
-#     BettingType.EXACTA: 5.7,
-#     BettingType.TRIO: 21.7,
-#     BettingType.TRIFECTA: 29.0,
-# }
-
-ratio_data_derby = {
-    # https://jra-van.jp/fun/baken/index3.html
-    BettingType.WIN: 6.3,
-    BettingType.QUINELLA: 16.3,
-    BettingType.EXACTA: 6.3,
-    BettingType.TRIO: 19.5,
-    BettingType.TRIFECTA: 37.1,
+ratio_data = {
+    # https://jra.jp/company/about/financial/pdf/houkoku03.pdf p.26 別表9
+    BettingType.WIN: 6.9,
+    BettingType.QUINELLA: 13.3,
+    BettingType.EXACTA: 5.7,
+    BettingType.TRIO: 21.7,
+    BettingType.TRIFECTA: 29.0,
 }
+
+# ratio_data_derby = {
+#     # https://jra-van.jp/fun/baken/index3.html
+#     BettingType.WIN: 6.3,
+#     BettingType.QUINELLA: 16.3,
+#     BettingType.EXACTA: 6.3,
+#     BettingType.TRIO: 19.5,
+#     BettingType.TRIFECTA: 37.1,
+# }
