ff9d8e7ed07f6924cab9d7f46c59f25a9853d4e5
Kiyofumi Watanabe <watanabe.kf1983@gmail.com>
2025-01-28 23:30:56 +0000

    EventBridge RuleとTargetの誤記を修正

--- infra/iac/modules/app/worker_scheduler.tf
+++ infra/iac/modules/app/worker_scheduler.tf
@@ -1,10 +1,10 @@
-resource "aws_cloudwatch_event_rule" "start_up_weekend_rule" {
+resource "aws_cloudwatch_event_rule" "start_up_weekday_rule" {
   name                = "${var.project_name}-${var.env}-start-up-worker-weekend"
   schedule_expression = "cron(00 22 ? * SUN-THU *)" # every MON-FRI 07:00(+09:00)
   tags                = var.common_tags
 }
 
-resource "aws_cloudwatch_event_rule" "start_up_weekday_rule" {
+resource "aws_cloudwatch_event_rule" "start_up_weekend_rule" {
   name                = "${var.project_name}-${var.env}-start-up-worker-weekday"
   schedule_expression = "cron(00 22 ? * FRI,SAT *)" # every SAT,SUN 07:00(+09:00)
   tags                = var.common_tags
@@ -33,7 +33,7 @@ resource "aws_cloudwatch_event_target" "start_up_weekday_target" {
 }
 
 resource "aws_cloudwatch_event_target" "start_up_weekend_target" {
-  rule = aws_cloudwatch_event_rule.start_up_weekday_rule.name
+  rule = aws_cloudwatch_event_rule.start_up_weekend_rule.name
   arn  = aws_lambda_function.ecs_task_scaler.arn
   input = jsonencode({
     "service_scales" : [
@@ -66,11 +66,11 @@ resource "aws_lambda_permission" "allow_eventbridge_start_up_weekend" {
 }
 
 resource "aws_lambda_permission" "allow_eventbridge_start_up_weekday" {
-  statement_id  = "AllowEventBridge-${var.env}-start-up-weekend"
+  statement_id  = "AllowEventBridge-${var.env}-start-up-weekday"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.ecs_task_scaler.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.start_up_weekend_rule.arn
+  source_arn    = aws_cloudwatch_event_rule.start_up_weekday_rule.arn
 }
 
 resource "aws_lambda_permission" "allow_eventbridge_shut_down" {
