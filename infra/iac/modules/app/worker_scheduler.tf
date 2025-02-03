eda1ea6d83e93a6452c4ddd71147741649aa34db
Kiyofumi Watanabe <watanabe.kf1983@gmail.com>
2025-02-03 03:53:12 +0000

    Change calc service task count

--- infra/iac/modules/app/worker_scheduler.tf
+++ infra/iac/modules/app/worker_scheduler.tf
@@ -38,7 +38,7 @@ resource "aws_cloudwatch_event_target" "start_up_weekend_target" {
   input = jsonencode({
     "service_scales" : [
       { "service_name" : "${aws_ecs_service.scrape_service.name}", "desired_task_count" : "1" },
-      { "service_name" : "${aws_ecs_service.calc_service.name}", "desired_task_count" : "3" }
+      { "service_name" : "${aws_ecs_service.calc_service.name}", "desired_task_count" : "2" }
   ] })
 }
 
