8b7ba04f61297d101a731e53029b920387bb161d
Kiyofumi Watanabe <watanabe.kf1983@gmail.com>
2025-06-24 13:36:22 +0000

    Fix error race at midnight

--- back/parimana/external/autorace/schedule/extract.py
+++ back/parimana/external/autorace/schedule/extract.py
@@ -12,11 +12,14 @@ def has_contents(page_html: str) -> bool:
         return False
 
 
-def extract_closing_time(page_html: str) -> datetime.time:
+def extract_closing_time(page_html: str) -> datetime.timedelta:
     soup = parse(page_html)
     race_text = soup.select_one("#race-result-current-race-telvote").get_text()
     if m := re.search(CLOSING_TIME_PATTERN, race_text):
-        return datetime.datetime.strptime(m.group("time"), "%H:%M").time()
+        # hour may be 24 or 25 at midnight
+        return datetime.timedelta(
+            hours=int(m.group("hour")), minutes=int(m.group("minute"))
+        )
     else:
         raise ValueError("Failed parse closing time string: " + race_text)
 
@@ -25,4 +28,6 @@ def parse(html: str) -> BeautifulSoup:
     return BeautifulSoup(html.encode("utf-8"), "html.parser", from_encoding="utf-8")
 
 
-CLOSING_TIME_PATTERN: re.Pattern = re.compile(r"投票締切\s*(?P<time>[0-9]{1,2}:[0-9]{2})")
+CLOSING_TIME_PATTERN: re.Pattern = re.compile(
+    r"投票締切\s*(?P<hour>[0-9]{1,2}):(?P<minute>[0-9]{2})"
+)
